6958a38c3265606354f38b016b1555b6a145de97
paranoid4ndro1d <marcio.morais96@outlook.com>
2022-04-26 17:51:11 -0600

    Criado função listarFilmesEmCartaz

--- cinema.js
+++ cinema.js
@@ -11,3 +11,14 @@ function listarTodosOsFilmes(filmes) {
   }
 }
 listarTodosOsFilmes(catalogoObj.data);
+
+function listarFilmesEmCartaz(filmes){
+  const filmesEmCartaz = filmes.filter((filme) => {
+    if (filme.emCartaz === true){
+      return true;
+    }
+  });
+  return filmesEmCartaz;
+}
+
+console.log(listarFilmesEmCartaz(catalogoObj.data));
